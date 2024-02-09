1c54904474864f8d6f1812dc79cab90c2318f414
amish <pithvaamish76@gmail.com>
2024-02-09 20:02:34 +0530

    contact compo improved

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -17,7 +17,15 @@ function Contact() {
       })
       .then(
         () => {
-          toast("🙌 Your message was sent Successfully!");
+          toast.success("Your message was sent Successfully!", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
         },
         (error) => {
           // console.log("FAILED...", error.text);
