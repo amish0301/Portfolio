77a870a15393cf67005b3b672be2a4b6147e7ca8
amish <pithvaamish76@gmail.com>
2024-03-09 00:05:21 +0530

    ScrollTop Button added

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,12 +1,32 @@
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import scrollImg from "../Assets/Arrow_top.png";
 
-function ScrollToTop() {
-  const { pathname } = useLocation();
+const ScrollToTop = () => {
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
-}
+    const handleScrollBtnVisibility = () => {
+      window.scrollY > 300
+        ? (document.querySelector(".scroll-to-top").style.display = "block")
+        : (document.querySelector(".scroll-to-top").style.display = "none");
+    };
+
+    window.addEventListener("scroll", handleScrollBtnVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", handleScrollBtnVisibility);
+    };
+  }, []);
+
+  return (
+    <>
+      <button className="scroll-to-top" onClick={handleScrollToTop}>
+        <img src={scrollImg} alt="scroll-to-top" />
+      </button>
+    </>
+  );
+};
 
 export default ScrollToTop;
