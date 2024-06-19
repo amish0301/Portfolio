b50835e66b8344ac41b9d59d36a9f04a742a232f
amish <pithvaamish76@gmail.com>
2024-06-19 21:09:01 +0530

    Resume added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-// import Resume from "./components/Resume/ResumeNew";
+import Resume from "./components/Resume/Resume";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -19,7 +19,6 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import NotFoundLayout from "./components/Resume/NotFoundLayout";
 
 function App() {
   const [load, upadateLoad] = useState(true);
@@ -43,7 +42,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<NotFoundLayout />} />
+          <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
