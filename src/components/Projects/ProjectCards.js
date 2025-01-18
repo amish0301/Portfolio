1ed95587b245531adf9544d5d8665b6f4badcac6
amish <pithvaamish76@gmail.com>
2025-01-18 14:31:39 +0530

    revert back to basic

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -7,41 +7,32 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <div
-        style={{ display: "block", overflow: "hidden", objectFit: "contain" }}
-      >
-        <Card.Img
-          variant="top"
-          src={props.imgPath}
-          alt="card-img"
-          height={"100%"}
-        />
-      </div>
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title style={{ marginBlock: ".5rem" }}>{props.title}</Card.Title>
-        <Card.Text
-          style={{ textAlign: "justify", textTransform: "capitalize", margin: '1rem 0' }}
-        >
+        <Card.Title>{props.title}</Card.Title>
+        <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <div
-          className="button-container"
-          style={{ display: "flex", justifyContent: "space-evenly" }}
-        >
-          <Button variant="primary" href={props.ghLink} target="_blank">
-            <BsGithub /> &nbsp;
-            {props.isBlog ? "Blog" : "GitHub"}
+        <Button variant="primary" href={props.ghLink} target="_blank">
+          <BsGithub /> &nbsp;
+          {props.isBlog ? "Blog" : "GitHub"}
+        </Button>
+        {"\n"}
+        {"\n"}
+
+        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+
+        {!props.isBlog && props.demoLink && (
+          <Button
+            variant="primary"
+            href={props.demoLink}
+            target="_blank"
+            style={{ marginLeft: "10px" }}
+          >
+            <CgWebsite /> &nbsp;
+            {"Demo"}
           </Button>
-
-          {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-          {!props.isBlog && props.demoLink && (
-            <Button variant="primary" href={props.demoLink} target="_blank">
-              <CgWebsite /> &nbsp;
-              {"Demo"}
-            </Button>
-          )}
-        </div>
+        )}
       </Card.Body>
     </Card>
   );
